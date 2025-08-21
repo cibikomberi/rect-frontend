1975268762829c590798eb7ca1b5908a83a06488
CIBI VISHNU A C <cibikomberi@gmail.com>
2025-08-21 16:34:11 +0530

    Update AdgeistBanner.js

--- src/Components/AdgeistBanner.js
+++ src/Components/AdgeistBanner.js
@@ -40,7 +40,7 @@ console.log()
             data-publisher-id={process.env.REACT_APP_ADGEIST_PUBLISHER_ID}
             data-api-key={process.env.REACT_APP_ADGEIST_API_KEY}
             data-ad-slot={process.env.REACT_APP_ADGEIST_AD_SLOT}
-            data-slot-type="banner"
+            data-slot-type="video"
             data-responsive="false"
             data-allowed-formats="jpg,jpeg,png,gif,mp4"
         />
